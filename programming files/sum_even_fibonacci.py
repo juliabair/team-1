f3c2fcd7dd4cfb35e93d23a93dbcefa8775a7316
NathanielBH <97924767+NathanielBH@users.noreply.github.com>
2023-10-24 10:23:27 -0400

    added comments to this one

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -1,10 +1,12 @@
 a, b = 0, 1
 sum_even = 0
-while b < 4000000:
+while b < 4000000: #while b is less than 4 billion
     if b % 2 == 0:
         sum_even += b
     a, b = b, a+b
 
-print(sum_even)
+print(sum_even) # printing the sum of even numbers
 
 # answer should be 4613732
+
+# otherwise program fails
