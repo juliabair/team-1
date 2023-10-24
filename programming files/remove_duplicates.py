03f13b815e2a8d9127e9002b6c5e02b143fc7f00
msutor1 <106191188+msutor1@users.noreply.github.com>
2023-10-24 10:33:18 -0400

    Update remove_duplicates.py

--- programming files/remove_duplicates.py
+++ programming files/remove_duplicates.py
@@ -1,7 +1,12 @@
+
+'''
+Function that sorts a list and removes duplicates
+'''
 def remove_duplicates(arr):
     arr.sort()
     unique_arr = []
     removed_elements = []
+    # Check if each element if unique, otherwise add it to a list of removed elements
     for i in range(len(arr)):
         if i == 0 or arr[i] != arr[i-1]:
             unique_arr.append(arr[i])
@@ -11,6 +16,7 @@ def remove_duplicates(arr):
 
 # Test the function
 arr = [1, 2, 2, 3, 4, 4, 5]
+# Print the original list, the list with duplicates removed, and the removed elements
 unique_arr, removed_elements = remove_duplicates(arr)
 print("Original array:", arr)
 print("Unique array:", unique_arr)
