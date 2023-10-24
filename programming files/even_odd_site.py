8b2350007cfcb7f95537298a2d76b5d648de085a
msutor1 <106191188+msutor1@users.noreply.github.com>
2023-10-24 10:24:53 -0400

    Update even_odd_site.py

--- programming files/even_odd_site.py
+++ programming files/even_odd_site.py
@@ -1,13 +1,17 @@
+# Open numbers file 
 with open("numbers.html", "w") as f:
+    # Create the header
     f.write("<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n")
     f.write("<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n")
+    # Determine if numbers 1-50 are even or odd and write them into the corresponding column 
     for i in range(1, 50):
         if i % 2 == 0:
             f.write("<tr><td>{}</td><td></td></tr>\n".format(i))
         else:
             f.write("<tr><td></td><td>{}</td></tr>\n".format(i))
     f.write("</table>\n</body>\n</html>")
-
+     
+# Print the contents of the file
 with open("numbers.html", "r") as f:
     print(f.read())
     
