601f86e864d7fbe4bfc4d9128fd9fe1207c6b719
NathanielBH <97924767+NathanielBH@users.noreply.github.com>
2023-10-24 10:26:19 -0400

    added comments for this one two

--- programming files/sum_even_fibonacci.java
+++ programming files/sum_even_fibonacci.java
@@ -1,7 +1,14 @@
+// summing even numbers in the fibonacci sequence
 public class sum_even_fibonacci {
+
+    // main function
     public static void main(String[] args) {
+        // initializing two integers
         int a = 0, b = 1;
+        //sum even initialized to 0
         int sumEven = 0;
+
+        // going through this loop and doing the fibinaci sequence for each one
         while (b < 4000000) {
             if (b % 2 == 0) {
                 sumEven += b;
@@ -10,6 +17,7 @@ public class sum_even_fibonacci {
             b = a + b;
             a = temp;
         }
+        // printing the number
         System.out.println(sumEven);
     }
 }
