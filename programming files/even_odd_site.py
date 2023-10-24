6fd6ad64bf1766a6d1e332bb96d8d75c35652d47
NathanielBH <97924767+NathanielBH@users.noreply.github.com>
2023-10-24 10:43:58 -0400

    DESTROYED even odds python

--- programming files/even_odd_site.py
+++ programming files/even_odd_site.py
@@ -1,11 +1,12 @@
 # Open numbers file 
+# bug introduced on line 9 doesn't check for even number: NBH
 with open("numbers.html", "w") as f:
     # Create the header
     f.write("<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n")
     f.write("<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n")
     # Determine if numbers 1-50 are even or odd and write them into the corresponding column 
     for i in range(1, 50):
-        if i % 2 == 0:
+        if i % 2 != 0:
             f.write("<tr><td>{}</td><td></td></tr>\n".format(i))
         else:
             f.write("<tr><td></td><td>{}</td></tr>\n".format(i))
