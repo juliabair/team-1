2eabb4cd3f0c3dfd741725d88489d1d16125930c
Alisa Hanada <123008646+alisahanada@users.noreply.github.com>
2023-10-24 10:31:29 -0400

    Update remove_duplicates.java

--- programming files/remove_duplicates.java
+++ programming files/remove_duplicates.java
@@ -4,15 +4,26 @@ import java.util.List;
 
 public class remove_duplicates {
 
+    // Function to remove duplicates from the input list 'arr' and keep track of removed elements
     public static List<Integer> removeDuplicates(List<Integer> arr, List<Integer> removedElements) {
+        
+        // Create a new list to store unique elements
         List<Integer> uniqueArr = new ArrayList<>();
+
+        // Iterate through the input list 'arr'
         for (int i = 0; i < arr.size(); i++) {
+
+            // Check if 'uniqueArr' does not contain the current element
             if (!uniqueArr.contains(arr.get(i))) {
+
+                // If not contained, add it to 'uniqueArr'
                 uniqueArr.add(arr.get(i));
             } else {
+                // If already contained, add it to the 'removedElements' list
                 removedElements.add(arr.get(i));
             }
         }
+        // Return the list of unique elements
         return uniqueArr;
     }
 
@@ -21,6 +32,8 @@ public class remove_duplicates {
         List<Integer> arr = Arrays.asList(1, 2, 2, 3, 4, 4, 5);
         List<Integer> removedElements = new ArrayList<>();
         List<Integer> uniqueArr = removeDuplicates(arr, removedElements);
+        
+        // Print the original array, unique array, and removed elements
         System.out.println("Original array: " + arr);
         System.out.println("Unique array: " + uniqueArr);
         System.out.println("Removed elements: " + removedElements);
