374e91689de3ac987f54d9ae3007a1ec67ea3a85
Julia Bair <121145356+juliabair@users.noreply.github.com>
2023-10-24 10:23:54 -0400

    Update rgb_to_hex.py

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -1,7 +1,10 @@
+# this method takes in three values, r, g, b for red, green, and blue
 def rgb_to_hex(r, g, b):
+    # each variable gives the min and max color values for each red, green, and blue
     r = max(0, min(255, r))
     g = max(0, min(255, g))
     b = max(0, min(255, b))
+    # returns each of the values formatted
     return '{:02X}{:02X}{:02X}'.format(r, g, b)
 
 
