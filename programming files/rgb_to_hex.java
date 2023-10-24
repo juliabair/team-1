5a1a43ddcd6392c74a3fc8d10b045edf950679cd
Julia Bair <121145356+juliabair@users.noreply.github.com>
2023-10-24 10:42:17 -0400

    Update rgb_to_hex.java

--- programming files/rgb_to_hex.java
+++ programming files/rgb_to_hex.java
@@ -1,9 +1,10 @@
+//bugs introduced: changed int r, g, b values; changed the rgbToHex minimum values from 255;   jb
 public class rgb_to_hex {
     public static void main(String[] args) {
         // define the RGB values
-        int r = 255;    // red component
-        int g = 127;    // green component
-        int b = 0;        // blue component
+        int r = 0;    // red component
+        int g = 67;    // green component
+        int b = 104;        // blue component
 
         // convert the RGB to the hexadecimal
         String hexColor = rgbToHex(r, g, b);
@@ -15,9 +16,9 @@ public class rgb_to_hex {
     public static String rgbToHex(int r, int g, int b) {
 
         // making sure that the values are within the valid range of 0-255 
-        r = Math.min(255, Math.max(0, r));
-        g = Math.min(255, Math.max(0, g));
-        b = Math.min(255, Math.max(0, b));
+        r = Math.min(200, Math.max(0, r));
+        g = Math.min(100, Math.max(0, g));
+        b = Math.min(230, Math.max(0, b));
 
         // convert the RBG to a hexadecimal string and format with leading zeros
         return String.format("%02X%02X%02X", r, g, b);
