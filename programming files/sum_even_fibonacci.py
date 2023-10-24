ccb1acd2c25cef795b38293fcbd65c35e09efe59
Julia Bair <121145356+juliabair@users.noreply.github.com>
2023-10-24 10:44:14 -0400

    Update sum_even_fibonacci.py

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -1,9 +1,10 @@
+#bugs introduced: changed the while less than value to 400; changed the last line of the while loop to a-b, it was a+b;   jb
 a, b = 0, 1
 sum_even = 0
-while b < 4000000: #while b is less than 4 billion
+while b < 400: #while b is less than 4 billion
     if b % 2 == 0:
         sum_even += b
-    a, b = b, a+b
+    a, b = b, a-b
 
 print(sum_even) # printing the sum of even numbers
 
